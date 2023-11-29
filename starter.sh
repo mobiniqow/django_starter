ce15a313d216d7d4515494f66f9aacc54fecee70
mobiniqow <41586263+mobiniqow@users.noreply.github.com>
2023-11-29 09:44:10 +0330

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -4,4 +4,4 @@ sudo  cp ./{{app_nginx_copnfig_name}}.config  /etc/nginx/sites-available/{{app_n
 sudo ln -sf /etc/nginx/sites-available/{{app_nginx_copnfig_name}}  /etc/nginx/sites-enabled/{{app_nginx_copnfig_name}}
 sudo  cp ./{{app_service_name}}.service  /etc/systemd/system/{{app_service_name}}.service
 sudo  cp ./{{app_socket_name}}.socket  /etc/systemd/system/{{app_socket_name}}.socket
-done
+echo "done"
