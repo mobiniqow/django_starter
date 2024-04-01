98e94358a52737c90ab1845a66703466756df748
mobiniqow <41586263+mobiniqow@users.noreply.github.com>
2024-04-01 08:17:33 +0330

    Update urls.py

--- {{cookiecutter.app_name}}/account/urls/v1/urls.py
+++ {{cookiecutter.app_name}}/account/urls/v1/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from account.urls.v1.views import (
-    RegisterAPIView,
     LoginAPIView,
     ProfileAPIView,
     VerifyAPIView,
@@ -13,7 +12,6 @@ app_name = "v1"
 urlpatterns = [
     path("verify/", VerifyAPIView.as_view(), name="verify"),
     path("profile/", ProfileAPIView.as_view(), name="profile"),
-    path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
