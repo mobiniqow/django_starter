a06c3080740e0274e5624f1f537e378155329a6d
mobiniqow <41586263+mobiniqow@users.noreply.github.com>
2024-04-02 09:32:02 +0330

    Update create_db.sh

--- {{cookiecutter.app_name}}/create_db.sh
+++ {{cookiecutter.app_name}}/create_db.sh
@@ -1,4 +1,5 @@
 # ساخت دیتابیس
+cd ~postgres/
 sudo -u postgres psql -c "CREATE DATABASE {{cookiecutter.DB_NAME}};"
 sudo -u postgres psql -c "CREATE USER {{cookiecutter.DB_USER}} WITH PASSWORD '{{cookiecutter.DB_PASSWORD}}';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE {{cookiecutter.DB_NAME}} TO {{cookiecutter.DB_USER}};"
